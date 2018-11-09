9e3cce5d722061b11ca9be38a0fd51910e2ca487
Reisen <reisen@morphism.org>
2018-11-09 14:15:36 +0000

    Finish reformatting import files.

--- src/Eventless.hs
+++ src/Eventless.hs
@@ -1,9 +1,10 @@
 module Eventless
   ( module Eventless
-  ) where
+  )
+where
 
-import Eventless.Commands           as Eventless
-import Eventless.Types.Aggregate    as Eventless
-import Eventless.Types.BackendStore as Eventless
-import Eventless.Types.Commands     as Eventless
-import Eventless.Types.Event        as Eventless
+import           Eventless.Commands            as Eventless
+import           Eventless.Types.Aggregate     as Eventless
+import           Eventless.Types.BackendStore  as Eventless
+import           Eventless.Types.Commands      as Eventless
+import           Eventless.Types.Event         as Eventless
